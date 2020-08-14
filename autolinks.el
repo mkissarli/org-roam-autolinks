efccaed687c1b859296e0273c9dc38195e55be1b
mkissarli <kissarlim@gmail.com>
2020-08-14 18:09:32 +0100

    Hopefully fixed bug with long lines with multiple links

--- autolinks.el
+++ autolinks.el
@@ -1,10 +1,7 @@
-[[test]][[propaganda]][[chomsky]]
-
 (defun strip-text-properties(txt)
   "Strips text properties from strings."
   (set-text-properties 0 (length txt) nil txt)
   txt)
-strip-text-properties
 
 (defun trim-link (link)
   (s-replace "[[" ""
@@ -12,7 +9,7 @@ strip-text-properties
 
 (defun get-links-in-buffer (buffer)
   (loop for x in (s-match-strings-all
-                  "\\[\\[.*]]"
+                  "\\[\\[.*?\\]\\]"
                   buffer)
         collect (strip-text-properties (first x))))
 
@@ -22,15 +19,13 @@ into account docstrings/code ect. i.e. [[link]]"
   (delete-dups
    (mapcar
     'trim-link
-    (flatten-list
-     (mapcar (lambda (x) (s-split
-                          "\\]\\]\\[\\["
-                          x))
-             (get-links-in-buffer buffer))))))
-
-(find-new-links (buffer-string))
-("test" "propaganda" "chomsky" "link")
-
+    (remove-if
+     (lambda (x) (s-contains? "][" x))
+     (flatten-list    
+      (mapcar (lambda (x) (s-split
+                           "\\]\\]\\[\\["
+                           x))
+              (get-links-in-buffer buffer)))))))
 
 (defun create-links-for-org-roam (links)
   (loop for x in links
@@ -41,4 +36,4 @@ into account docstrings/code ect. i.e. [[link]]"
 (defun create-links-for-org-roam-buffer ()
   (create-links-for-org-roam (find-new-links (buffer-string))))
 
-(create-links-for-org-roam-buffer)
+(find-new-links (buffer-string))
